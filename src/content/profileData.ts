a35edfbc191d93318a8591959ae06ea4b7f13077
Vaibhav <sharmavaibhav227@gmail.com>
2025-01-24 01:44:20 +0530

    portfolio

---
+++ src/content/profileData.ts
@@ -0,0 +1,53 @@
+export const PROFILE = {
+  site: {
+    SEO: {
+      //     more complicated description for SEO
+      title: "Vaibhav Sharma (@Vaibhav227) - Learn, Share and Grow ❤️",
+      description:
+        "Learn, Share and Grow ❤️ -- Software Engineer | AWS Professional | Open Source Lover  --☘️ Be more productive, Better in communicating, Contribute more to community. | Vaibhav's New Astro Portfolio",
+    },
+  },
+  timezone: "Asia/Kolkata",
+  language: "en-IN",
+  firstName: "Vaibhav",
+  name: "Vaibhav Sharma",
+  headLine: "Learn, Share and Grow ❤️",
+  headLine2:
+    "I enjoy providing software solutions and bring ideas to life and solve real-world problems.",
+  website: "https://vaibhu.com",
+  repo: "https://github.com/Vaibhav227/portfolio",
+  avatarUrl: "https://vaibhu.com/vaibhu.webp",
+  shortIntros: [
+    "💻 Software/AI Engineer | Open Source Lover ",
+    "☘️ Passionate about Dev Experience(DX) Improvement and bringing ideas to life",
+    "📈 Unprofessional Investor",
+    "🌱 Part-time blogger and unprofessional Youtuber",
+    "📚 Book reader | 🎧 Music Lover | Chess Player",
+  ],
+  words: ["As each one has received a gift, minister it to one another"],
+  skills: [
+    "Remix",
+    "Typescript",
+    "React",
+    "Cloud Computing",
+    "Kubernetes",
+    "Docker",
+    "Node.js",
+    "Postgres",
+    "SQL",
+    "Astro",
+  ],
+  links: {
+    github: "https://github.com/Vaibhav227",
+    mail: "mailto:sharmavaibhav227@gmail.com",
+    linkedin: "https://www.linkedin.com/in/vaibhav-sharma-844847204/",
+    instagram: "https://www.instagram.com/sharmavaibhav227/",
+  },
+  studies: [
+    // studies, qualifications and certifications
+    {
+      title: "Bachelor of Technology in Computer Science and Engineering",
+      level: "Bachelor",
+    },
+  ],
+};
