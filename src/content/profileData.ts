7b34b92aaff02acc2f50d0eb1133daa2b6a54a3b
Vaibhav <sharmavaibhav227@gmail.com>
2025-02-02 11:36:13 +0530

    updated profile

--- src/content/profileData.ts
+++ src/content/profileData.ts
@@ -4,27 +4,28 @@ export const PROFILE = {
       //     more complicated description for SEO
       title: "Vaibhav Sharma (@Vaibhav227) - Learn, Share and Grow ❤️",
       description:
-        "Learn, Share and Grow ❤️ -- Software Engineer | AWS Professional | Open Source Lover  --☘️ Be more productive, Better in communicating, Contribute more to community. | Vaibhav's New Astro Portfolio",
+        "Learn and Grow ❤️ -- Software Engineer | Open Source Lover  --☘️ Be more productive, Better in communicating, Contribute more to community. | Vaibhav's New Astro Portfolio",
     },
   },
   timezone: "Asia/Kolkata",
   language: "en-IN",
   firstName: "Vaibhav",
   name: "Vaibhav Sharma",
-  headLine: "Learn, Share and Grow ❤️",
+  headLine: "Learn and Grow ❤️",
   headLine2:
-    "I enjoy providing software solutions and bring ideas to life and solve real-world problems.",
+    "I enjoy building software and bring ideas to life and solve real-world problems.",
   website: "https://vaibhu.com",
   repo: "https://github.com/Vaibhav227/portfolio",
   // avatarUrl: "https://vaibhu.com/vaibhu.webp",
   shortIntros: [
-    "💻 Software/AI Engineer | Open Source Lover ",
-    "☘️ Passionate about Dev Experience(DX) Improvement and bringing ideas to life",
+    "💻 Software Engineer | Open Source Lover ",
+    "☘️ Passionate about User Experience(UX) Improvement and bringing ideas to life",
     "📈 Unprofessional Investor",
-    "🌱 Part-time blogger and unprofessional Youtuber",
-    "📚 Book reader | 🎧 Music Lover | Chess Player",
+    "🎧 Music Lover | Love Playing Chess",
   ],
-  words: ["As each one has received a gift, minister it to one another"],
+  words: [
+    "Life without love is useless and poor they say, appoint me in thy service, O Lord, with Love as my pay. - Srila Bhaktisiddhanta Sarasvati Thakur",
+  ],
   skills: [
     "Remix",
     "Typescript",
